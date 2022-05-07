f778a6bbdf21d5d578a235915a7e7ca78a5f36cf
Gwen Ewasko <gwenewasko@gmail.com>
2022-05-07 17:43:14 -0500

    nav and some home page

--- gwenewasko/src/components/Header.js
+++ gwenewasko/src/components/Header.js
@@ -1,8 +1,41 @@
+import Logo from "../img/Gwen-logomark.png";
+
 export default function Header(props) {
   return (
     <>
       <header>
-        <nav>home work about</nav>
+        <nav class="ai-c bgc-white d-f jc-sb p sticky d-f@lrg">
+          <div>
+            <img class="logo w-50" src={Logo} alt="Gwen Ewasko" />
+          </div>
+          <ul class="ai-c d-f fxw-w lis-n m menu m0 p">
+            <li class="underline ml@lrg nav-link">
+              <a class="c-lightgray" href="#aboutme">
+                About Me
+              </a>
+            </li>
+            <li class="underline ml@lrg nav-link">
+              <a class="c-lightgray" href="#work">
+                Work
+              </a>
+            </li>
+            <li class="underline ml@lrg nav-link">
+              <a class="c-lightgray" href="#resume">
+                Resume
+              </a>
+            </li>
+            <li class="underline ml@lrg nav-link">
+              <a class="c-lightgray" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+          <div class="hamburger">
+            <span class="hamburger-bar"></span>
+            <span class="hamburger-bar"></span>
+            <span class="hamburger-bar"></span>
+          </div>
+        </nav>
       </header>
     </>
   );
--- gwenewasko/src/pages/Home.js
+++ gwenewasko/src/pages/Home.js
@@ -1,5 +1,16 @@
+import Headshot from "../img/gwen-cutout_crop-sm.png";
+
 function Home(props) {
-  return <>Hi Home</>;
+  return (
+    <>
+      <div>
+        <img class="pos-b w-50" src={Headshot} alt="Gwen's Headshot" />
+      </div>
+      <h3 class="f-oswald f-400 f-size">
+        That's me! I'm a designer and full stack developer.
+      </h3>
+    </>
+  );
 }
 
 export default Home;
